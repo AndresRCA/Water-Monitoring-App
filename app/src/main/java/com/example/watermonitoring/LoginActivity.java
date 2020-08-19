82717cbe0be3ed621352ad0b6ab75e1e161ef98c
Andrew <andrescastillo.r@hotmail.com>
2020-08-18 23:33:50 -0400

    migrate to androidx

--- app/src/main/java/com/example/watermonitoring/LoginActivity.java
+++ app/src/main/java/com/example/watermonitoring/LoginActivity.java
@@ -4,10 +4,8 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
 import android.content.CursorLoader;
@@ -32,8 +30,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via username/password.
