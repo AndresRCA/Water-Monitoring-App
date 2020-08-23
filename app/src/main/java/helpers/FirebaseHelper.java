69e6a204ef225393707ecc1242f4c6d34e457c5d
Andrew <andrescastillo.r@hotmail.com>
2020-08-23 13:10:04 -0400

    Firebase implementation V.1

--- app/src/main/java/helpers/FirebaseHelper.java
+++ app/src/main/java/helpers/FirebaseHelper.java
@@ -7,9 +7,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,37 +20,37 @@ import models.WaterSample;
 public class FirebaseHelper {
     private FirebaseDatabase db;
     private DatabaseReference waterSamples;
-    public ArrayList<WaterSample> water_set;
+    private Query monthlyWaterSamples; // previous month water samples
+    public ArrayList<WaterSample> water_set; // array that is displayed on the activity in a graph
 
-    public FirebaseHelper(String username/*, String password*/) { // password is used only in validateUser(), delete it when moving it to standalone function
+    public FirebaseHelper(String username) {
         db = FirebaseDatabase.getInstance();
-        //validateUser(username, password); /* this will probably be a standalone function in the login page, no need to add it to the helper class */
-        /*DatabaseReference user = db.getReference("/users").orderByChild("username").equalTo(username).getRef(); // get reference to the user with a given username value
-        waterSamples = user.child("waterSamples"); // get the waterSamples reference for this user
+
+        // set waterSamples
+        DatabaseReference user = db.getReference("/users/" + username);
+        waterSamples = user.child("waterSamples"); // get the waterSamples reference for this user, this property is used for monthlyWaterSamples, and is not really used directly
+
+        // set monthlyWaterSamples
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, -1);
+        long last_month = calendar.getTimeInMillis();
+        monthlyWaterSamples = waterSamples.orderByChild("created_at").startAt(last_month);
+
         setInitialWaterSet(); // initialize water_set
         setEventListeners(); // listen to changes in the waterSamples ref and update the water_set in the app
-        */
     }
-
-    /* this will probably be a standalone function in the login page, no need to add it to the helper class */
-	/*private void validateUser(String user, String password) {
-		// ...
-	}*/
 
     // retrieve initial array of water samples
     private void setInitialWaterSet() {
         water_set = new ArrayList<WaterSample>();
-        waterSamples.addListenerForSingleValueEvent(new ValueEventListener() {
+        monthlyWaterSamples.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.i("onDataChange", dataSnapshot.toString());
-                if (dataSnapshot.hasChildren()) { // if /user/$user/waterSamples has children (samples)
-                    /* try to limit the number of children beforehand, in ref */
-                    // check the benefit of adding .iterator() to .getChildren(), since there could be unneeded data (only need a month's time of data):
-                    // benefit: If you are using for loop you cannot update(add/remove) the Collection whereas with the help of an iterator you can easily update Collection
-                    // this benefit is probably useless in this case since I'm creating a new array, not reusing the getChildren() iterable
+                if (dataSnapshot.hasChildren() && dataSnapshot.exists()) { // if /user/$user/waterSamples has children (samples)
                     for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        Log.i("onDataChange", dataSnapshot.toString());
                         WaterSample water_sample = child.getValue(WaterSample.class);
+                        water_sample.setKey(child.getKey());
                         water_set.add(water_sample);
                     }
                 }
@@ -64,12 +66,13 @@ public class FirebaseHelper {
 
     // set event listeners for the water_set used in the graph
     private void setEventListeners() {
-        ChildEventListener childEventListener = new ChildEventListener() {
+        monthlyWaterSamples.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
-                Log.d("onChildAdded", "onChildAdded:" + dataSnapshot.getKey());
+                Log.d("onChildAdded", dataSnapshot.getKey());
                 // A new water sample has been added
                 WaterSample sample = dataSnapshot.getValue(WaterSample.class);
+                sample.setKey(dataSnapshot.getKey());
                 water_set.add(sample);
             }
 
@@ -79,7 +82,7 @@ public class FirebaseHelper {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-                Log.d("onChildRemoved", "onChildRemoved:" + dataSnapshot.getKey());
+                Log.d("onChildRemoved", dataSnapshot.getKey());
                 // Do something like get the id (key) and compare it to something in water_set and remove it, or just remove the first element since the first element will always be the one getting removed according to cloud functions
                 water_set.remove(0);
             }
@@ -91,9 +94,7 @@ public class FirebaseHelper {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
-        };
-
-        waterSamples.addChildEventListener(childEventListener);
+        });
     }
 
 }
--- app/src/main/java/models/WaterSample.java
+++ app/src/main/java/models/WaterSample.java
@@ -1,20 +1,34 @@
 package models;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class WaterSample {
-    //public String key; // the key given by push() in firebase
-    public String pH;
-    public String orp;
-    public String turbidity;
+    public String key; // the key given by push() in firebase
+    public long created_at;
+    public double pH;
+    public double orp;
+    public double turbidity;
 
     public WaterSample() {
         // Default constructor required for calls to DataSnapshot.getValue(WaterSample.class)
     }
 
-    public WaterSample(/*String key, */String ph, String orp, String turbidity) {
-        //this.key = key;
+    public WaterSample(long created_at, double ph, double orp, double turbidity) {
+        this.created_at = created_at;
         this.pH = ph;
         this.orp = orp;
         this.turbidity = turbidity;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getStrDate(String format) {
+        DateFormat formatter = new SimpleDateFormat(format);
+        String str_date = formatter.format(created_at);
+        return str_date;
     }
 
 }
